6f7c009157a97e25a3a08f6d0e0e267eef9d7e22
Alex Aunter <lxaunter@gmail.com>
2022-02-12 16:49:10 +0300

    feat(shoes): add togglable url params

--- src/components/brand_link.jsx
+++ src/components/brand_link.jsx
@@ -2,11 +2,22 @@ import { useSearchParams, Link } from "react-router-dom";
 
 export default function BrandLink({ brand, ...props }) {
   let [params] = useSearchParams();
-  let isActive = params.getAll("brand").includes(brand);
+  let brands = params.getAll("brand");
+  let isActive =
+    brands.includes(brand);
+  if (!isActive) {
+    params.append("brand", brand);
+  } else {
+    params = new URLSearchParams(
+      Array.from(params).filter(
+        ([key, value]) => key !== "brand" || value !== brand
+      )
+    );
+  }
   return (
     <Link
       className={isActive ? "red" : "" }
-      to={`/shoes?brand=${brand}`}
+      to={`/shoes?${params.toString()}`}
       {...props}
     />
   );
