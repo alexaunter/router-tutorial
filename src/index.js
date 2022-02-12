5bf99a883c023cb2dd84abeb58516ea68f24f588
Alex Aunter <lxaunter@gmail.com>
2022-02-12 12:49:30 +0300

    feat(router): add basing params rendering

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import {
 import App from './App';
 import Expenses from "./routes/expenses";
 import Invoices from "./routes/invoices";
+import Invoice from "./routes/invoice";
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
@@ -17,7 +18,17 @@ ReactDOM.render(
       <Routes>
         <Route path="/" element={<App />}>
           <Route path="expenses" element={<Expenses />} />
-          <Route path="invoices" element={<Invoices />} />
+          <Route path="invoices" element={<Invoices />}>
+            <Route path=":invoiceId" element={<Invoice />} />
+          </Route>
+          <Route
+            path="*"
+            element={
+              <main style={{ padding: "1rem" }}>
+                <p>There's nothing here!</p>
+              </main>
+            }
+          />
         </Route>
       </Routes>
     </BrowserRouter>
